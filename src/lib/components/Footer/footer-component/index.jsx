b59af22480d78b74e6080d407feff379eeef00e7
Cao Xi <1060010408@qq.com>
2024-04-10 18:05:20 +0800

    fix: remove image url

--- src/lib/components/Footer/footer-component/index.jsx
+++ src/lib/components/Footer/footer-component/index.jsx
@@ -204,50 +204,50 @@ const mobileMenuOrder = ['keystone', 'partnership', 'legal']
 export const Footer = () => {
   const intl = useIntl()
   const isZh = useMemo(() => intl.locale === 'zh', [intl.locale])
-  const icons = [
-    {
-      title: 'Discord',
-      icon: new URL('../images/discord.png', import.meta.url).href,
-      activeIcon: new URL('../images/discord_c.png', import.meta.url).href,
-      link: 'https://keyst.one/discord',
-    },
-    {
-      title: 'Twitter',
-      icon: new URL('../images/x.png', import.meta.url).href,
-      activeIcon: new URL('../images/x_c.png', import.meta.url).href,
-      link: isZh ? 'https://twitter.com/KeystoneCN' : 'https://twitter.com/KeystoneWallet',
-    },
-    {
-      title: 'Medium',
-      icon: new URL('../images/medium.png', import.meta.url).href,
-      activeIcon: new URL('../images/medium_c.png', import.meta.url).href,
-      link: 'https://blog.keyst.one',
-    },
-    {
-      title: 'YouTube',
-      icon: new URL('../images/youtube.png', import.meta.url).href,
-      activeIcon: new URL('../images/youtube_c.png', import.meta.url).href,
-      link: 'https://www.youtube.com/channel/UCaReIdawwYPPcyWGoNunF7g',
-    },
-    {
-      title: 'Telegram',
-      icon: new URL('../images/telegram.png', import.meta.url).href,
-      activeIcon: new URL('../images/telegram_c.png', import.meta.url).href,
-      link: isZh ? 'https://t.me/KeystoneWalletCN' : 'https://t.me/KeystoneWallet',
-    },
-    {
-      title: '即刻',
-      icon: new URL('../images/jike.png', import.meta.url).href,
-      activeIcon: new URL('../images/jike_c.png', import.meta.url).href,
-      link: 'https://web.okjike.com/u/370FC219-7DFC-4774-BC01-3AE19347D1E7',
-    },
-    {
-      title: 'Reddit',
-      icon: new URL('../images/reddit.png', import.meta.url).href,
-      activeIcon: new URL('../images/reddit_c.png', import.meta.url).href,
-      link: 'https://www.reddit.com/r/KeystoneWallet/',
-    },
-  ]
+  // const icons = [
+  //   {
+  //     title: 'Discord',
+  //     icon: new URL('../images/discord.png', import.meta.url).href,
+  //     activeIcon: new URL('../images/discord_c.png', import.meta.url).href,
+  //     link: 'https://keyst.one/discord',
+  //   },
+  //   {
+  //     title: 'Twitter',
+  //     icon: new URL('../images/x.png', import.meta.url).href,
+  //     activeIcon: new URL('../images/x_c.png', import.meta.url).href,
+  //     link: isZh ? 'https://twitter.com/KeystoneCN' : 'https://twitter.com/KeystoneWallet',
+  //   },
+  //   {
+  //     title: 'Medium',
+  //     icon: new URL('../images/medium.png', import.meta.url).href,
+  //     activeIcon: new URL('../images/medium_c.png', import.meta.url).href,
+  //     link: 'https://blog.keyst.one',
+  //   },
+  //   {
+  //     title: 'YouTube',
+  //     icon: new URL('../images/youtube.png', import.meta.url).href,
+  //     activeIcon: new URL('../images/youtube_c.png', import.meta.url).href,
+  //     link: 'https://www.youtube.com/channel/UCaReIdawwYPPcyWGoNunF7g',
+  //   },
+  //   {
+  //     title: 'Telegram',
+  //     icon: new URL('../images/telegram.png', import.meta.url).href,
+  //     activeIcon: new URL('../images/telegram_c.png', import.meta.url).href,
+  //     link: isZh ? 'https://t.me/KeystoneWalletCN' : 'https://t.me/KeystoneWallet',
+  //   },
+  //   {
+  //     title: '即刻',
+  //     icon: new URL('../images/jike.png', import.meta.url).href,
+  //     activeIcon: new URL('../images/jike_c.png', import.meta.url).href,
+  //     link: 'https://web.okjike.com/u/370FC219-7DFC-4774-BC01-3AE19347D1E7',
+  //   },
+  //   {
+  //     title: 'Reddit',
+  //     icon: new URL('../images/reddit.png', import.meta.url).href,
+  //     activeIcon: new URL('../images/reddit_c.png', import.meta.url).href,
+  //     link: 'https://www.reddit.com/r/KeystoneWallet/',
+  //   },
+  // ]
 
   const isMobile = useIsMobile()
   const menu = useMemo(() => {
@@ -263,23 +263,23 @@ export const Footer = () => {
     })
   }
 
-  const medias = icons.map((e) => (
-    <Media
-      target="_blank"
-      href={e.link}
-      key={e.title}
-      className={`media-${e.title}`}
-      data-background-color="var(--color-bg-slight)"
-      data-text-color="var(--color-fg-default)"
-      data-mixpanel-id={e.title}
-      data-mixpanel-pos="footer"
-      data-mixpanel-to={e.link}
-    >
-      <img src={e.icon} className="default" alt={e.title} loading="lazy" />
-      <img src={e.activeIcon} className="active" alt={e.title} loading="lazy" />
-      <Tooltip anchorSelect={`.media-${e.title}`} content={e.title} />
-    </Media>
-  ))
+  // const medias = icons.map((e) => (
+  //   <Media
+  //     target="_blank"
+  //     href={e.link}
+  //     key={e.title}
+  //     className={`media-${e.title}`}
+  //     data-background-color="var(--color-bg-slight)"
+  //     data-text-color="var(--color-fg-default)"
+  //     data-mixpanel-id={e.title}
+  //     data-mixpanel-pos="footer"
+  //     data-mixpanel-to={e.link}
+  //   >
+  //     <img src={e.icon} className="default" alt={e.title} loading="lazy" />
+  //     <img src={e.activeIcon} className="active" alt={e.title} loading="lazy" />
+  //     <Tooltip anchorSelect={`.media-${e.title}`} content={e.title} />
+  //   </Media>
+  // ))
 
   const categories = menu.map((it) =>
     it.map((col) => ({
@@ -305,7 +305,7 @@ export const Footer = () => {
       <Container>
         <Main>
           <MainLeft>
-            <Logo src={new URL('../images/logo.png', import.meta.url).href} loading="lazy" />
+            {/* <Logo src={new URL('../images/logo.png', import.meta.url).href} loading="lazy" /> */}
             <Slogan>
               <FormattedMessage {...messages.slogan} values={{ br: <br /> }} />
             </Slogan>
@@ -353,7 +353,7 @@ export const Footer = () => {
               <FormattedMessage {...messages.orderTrack} />
             </FooterLinkItem>
           </UtilsAreaLeft>
-          <MediaList>{medias}</MediaList>
+          {/* <MediaList>{medias}</MediaList> */}
         </UtilsArea>
         <Copyright>Keystone © All rights reserved 2018 - {new Date().getFullYear()}</Copyright>
       </Container>
