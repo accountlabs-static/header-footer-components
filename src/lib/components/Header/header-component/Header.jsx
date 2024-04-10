cda88c249f90353145a51b854ad272ba154b3f98
Cao Xi <1060010408@qq.com>
2024-04-10 11:09:25 +0800

    fix: use named export

--- src/lib/components/Header/header-component/Header.jsx
+++ src/lib/components/Header/header-component/Header.jsx
@@ -5,7 +5,7 @@ import messages from '../../translations/en.json'
 import { Header as HeaderComponent } from './index'
 import '../../index.scss'
 
-function Header() {
+export function Header() {
   return (
     <BrowserRouter>
       <IntlProvider locale="en" messages={messages}>
@@ -14,5 +14,3 @@ function Header() {
     </BrowserRouter>
   )
 }
-
-export default Header
